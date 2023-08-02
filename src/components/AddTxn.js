074225db337ef77fe68663ede052a039de9a9909
Abhay Kamath <abhaykamath2299@gmail.com>
2023-08-02 11:15:04 +0530

    Reverted date fix

--- src/components/AddTxn.js
+++ src/components/AddTxn.js
@@ -14,7 +14,6 @@ function AddTxn({ descRef, amountRef, dateRef, updateDate }) {
           updateDate(e.target.value);
         }}
         type="date"
-        placeholder="dd-mm-yyy"
       />
       <div className="lbl lbl-main">Amount</div>
       <input ref={amountRef} type="number" placeholder="Enter Amount" />
